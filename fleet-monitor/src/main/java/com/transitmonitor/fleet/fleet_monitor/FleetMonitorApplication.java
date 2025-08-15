1e6baf822e051103759b29cfe767da3814a6cd20
ritav <dummy@example.com>
2025-08-15 21:55:10 +0530

    Enabled Scheduling

--- fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplication.java
+++ fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplication.java
@@ -2,8 +2,10 @@ package com.transitmonitor.fleet.fleet_monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FleetMonitorApplication {
 
 	public static void main(String[] args) {
--- fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/service/GtfsRealtimeService.java
+++ fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/service/GtfsRealtimeService.java
@@ -8,16 +8,19 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GtfsRealtimeService {
+    //TODO: Refactor w/ @Value based external configuration later on
     private static final String GTFS_RT_FEED_URL = "http://data.itsfactory.fi/journeys/api/1/gtfs-rt/vehicle-positions";
     private final HttpClient httpClient;
     public GtfsRealtimeService() {
         this.httpClient = HttpClient.newHttpClient();
     }
 
+    @Scheduled(fixedRate = 30000)
     public void fetchGtfsRealtimeData() {
         try {
             HttpRequest request = HttpRequest.newBuilder(new URI(GTFS_RT_FEED_URL)).GET().build();
