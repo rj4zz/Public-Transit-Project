cd11aceba1a8682dd52623fad773047aa6e35f7c
ritav <dummy@example.com>
2025-08-28 22:45:14 +0530

    Created a REST API endpoint for displaying anomalies

--- fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/controller/FleetMonitorController.java
+++ fleet-monitor/src/main/java/com/transitmonitor/fleet/fleet_monitor/controller/FleetMonitorController.java
@@ -1,18 +1,32 @@
 package com.transitmonitor.fleet.fleet_monitor.controller;
 
 import java.time.Instant;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.transitmonitor.fleet.fleet_monitor.entity.AnomalyEntity;
 import com.transitmonitor.fleet.fleet_monitor.model.SystemStatus;
+import com.transitmonitor.fleet.fleet_monitor.repository.AnomalyRepository;
 
 
 @RestController
 public class FleetMonitorController {
+    private final AnomalyRepository anomalyRepository;
+
+    public FleetMonitorController(AnomalyRepository anomalyRepository) {
+        this.anomalyRepository = anomalyRepository;
+    }
+
     @GetMapping("/api/status")
     public SystemStatus getSystemStatus() {
         Instant now = Instant.now();
         return new SystemStatus("System is running", true, now);
+    }
+
+    @GetMapping("/api/anomalies")
+    public List<AnomalyEntity> getAllAnomalies() {
+        return anomalyRepository.findAll();
     }
 }
