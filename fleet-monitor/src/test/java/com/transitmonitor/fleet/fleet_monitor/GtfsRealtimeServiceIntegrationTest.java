d1910270d7e79b2e2220a9a71abf456d47d03c9f
ritav <dummy@example.com>
2025-09-08 02:10:58 +0530

    Add GtfsRealtimeService integration test which tests the fetch of realtime data

--- fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/GtfsRealtimeServiceIntegrationTest.java
+++ fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/GtfsRealtimeServiceIntegrationTest.java
@@ -1,11 +1,14 @@
 package com.transitmonitor.fleet.fleet_monitor;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import com.transitmonitor.fleet.fleet_monitor.repository.VehiclePositionRepository;
 import com.transitmonitor.fleet.fleet_monitor.service.GtfsRealtimeService;
@@ -15,12 +18,20 @@ import com.transitmonitor.fleet.fleet_monitor.service.GtfsRealtimeService;
 public class GtfsRealtimeServiceIntegrationTest {
     @Container
     @ServiceConnection
-    private static PostgreSQLContainer<?> dbContainer = new PostgreSQLContainer<>("postgis/postgis:latest");
+    private static PostgreSQLContainer<?> dbContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgis/postgis:latest").asCompatibleSubstituteFor("postgres"));
     
     @Autowired
     private GtfsRealtimeService gtfsRealtimeService;
 
     @Autowired
     private VehiclePositionRepository vehiclePositionRepository;
+
+    @Test
+    public void testFetchAndSaveGtfsRealtimeData() throws InterruptedException {
+        gtfsRealtimeService.fetchGtfsRealtimeData();
+        Thread.sleep(50000);
+        int count = (int) vehiclePositionRepository.count();
+        Assertions.assertTrue(count > 0);
+    }
 
 }
