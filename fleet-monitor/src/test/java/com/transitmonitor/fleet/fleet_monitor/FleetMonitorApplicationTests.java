d1d6c33e1d30fd5697db00fd877fa4d9a4cd8eeb
ritav <dummy@example.com>
2025-09-20 19:35:04 +0530

    Configured the database platform for FleetMonitorApplicationTest to H2

--- fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplicationTests.java
+++ fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 @AutoConfigureTestDatabase(replace = Replace.ANY)
 class FleetMonitorApplicationTests {
 
