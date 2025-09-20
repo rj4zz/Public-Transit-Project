d00389a3f26d9cba52cce70a5833fe5781fd4e66
ritav <dummy@example.com>
2025-09-20 15:58:43 +0530

    Configured test database for FleetMonitorApplicationTests

--- fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplicationTests.java
+++ fleet-monitor/src/test/java/com/transitmonitor/fleet/fleet_monitor/FleetMonitorApplicationTests.java
@@ -1,9 +1,12 @@
 package com.transitmonitor.fleet.fleet_monitor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class FleetMonitorApplicationTests {
 
 	@Test
